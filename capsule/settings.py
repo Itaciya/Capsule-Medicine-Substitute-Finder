dee7265a047808a3462c35e34fe1f1849905fae7
Itaciya <psfq606@gmail.com>
2026-05-20 22:30:31 +0600

    set DEBUG to False for production

--- capsule/settings.py
+++ capsule/settings.py
@@ -6,9 +6,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production-!@#'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com"
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
